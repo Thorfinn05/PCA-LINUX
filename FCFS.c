70db566da492327150a6a3e83e6deefd7e7c46a0
Rudranil Das <rudranild04@gmail.com>
2025-11-02 19:43:36 +0530

    Update FCFS.c

--- FCFS.c
+++ FCFS.c
@@ -18,15 +18,19 @@ int main(){
 	for(int i=1; i<n; i++){
 		wt[i] = wt[i-1] + bt[i-1];
 	}
+	int tat[n];
+	for(int i=1; i<n; i++){
+		tat[i] = wt[i] + bt[i];
+	}
 	printf("PID  BT  WT  TAT\n");
 	float twt = 0.0;
-	float tat = 0.0;
+	float ttat = 0.0;
 	for(int i=0; i<n; i++){
-		printf("%d\t\t%d\t\t%d\t\t%d\n", pid[i], bt[i], wt[i], bt[i]+wt[i]);
+		printf("%d\t\t%d\t\t%d\t\t%d\n", pid[i], bt[i], wt[i], tat[i]);
 		twt += wt[i];
-		tat += (wt[i]+bt[i]);
+		ttat += tat[i];
 	}
 	printf("Avg WT: %2f", twt/n);
-	printf("Avg TAT: %2f", tat/n);
+	printf("Avg TAT: %2f", ttat/n);
 	return 0;
 }
